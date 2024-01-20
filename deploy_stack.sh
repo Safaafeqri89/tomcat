2662ad40eae4c579c9d0094090399283052e33d6
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-20 23:30:35 +0100

    deploy_stack.sh

---
+++ deploy_stack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Nombre del stack
+STACK_NAME="instanciaYML"
+
+# Nombre del archivo YAML que contiene la plantilla
+TEMPLATE_FILE="https://github.com/Safaafeqri89/tomcat/blob/main/main.yml"
+
+# Nombre del archivo de parámetros (si es necesario)
+
+# Crear o actualizar el stack
+aws cloudformation deploy \
+  --stack-name $STACK_NAME \
+  --template-file $TEMPLATE_FILE \
+  --capabilities CAPABILITY_IAM
