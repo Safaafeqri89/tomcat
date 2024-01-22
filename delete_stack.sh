a6179243b8adf2ca704971d3072d23acec1f05d6
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-22 16:20:39 +0100

    delete_stack.sh

--- delete_stack.sh
+++ delete_stack.sh
@@ -5,5 +5,5 @@ aws cloudformation delete-stack \
 --stack-name "instanciaYML"  \
 # Esperar hasta que la eliminación se complete
 aws cloudformation wait stack-delete-complete
---stack-name "instanciaYML"
+--stack-name "instanciadesdeCLI"
 
