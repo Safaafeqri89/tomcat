b3b5d8f5ff6fe40922e7da00b2e57640fb7a65c3
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-22 23:58:29 +0100

    script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#desplegamos un stack y  Nombre del archivo YAML que contiene la plantilla
+#desplegamos un stack y Nombre del archivo YAML que contiene la plantilla
 # Crear o actualizar el stack
 aws cloudformation deploy \
   --template-file tomcat/main.yml \
