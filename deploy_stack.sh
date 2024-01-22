b84a67dfe01408a209023423fe2c267986203024
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-22 13:24:08 +0100

    deploy_stack.sh

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -3,6 +3,6 @@
 # Desplegamos la plantilla de CloudFormation en base a nuestro fichero YAML, establecemos el nombre de la pila y 
 # establecemos  las capacidades de nuestra pila
 aws cloudformation deploy \
---template-file main.yml \
+--template-file tomcat/main.yml \
 --stack-name "instanciaYML" \
 --capabilities CAPABILITY_NAMED_IAM
