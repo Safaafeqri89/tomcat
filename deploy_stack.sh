38b6e9d067aa62908478839f58aa310f5ce34e15
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-23 20:27:00 +0100

    deploy_stack.sh

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -3,7 +3,7 @@
 #desplegamos un stack y  Nombre del archivo YAML que contiene la plantilla
 # Crear o actualizar el stack
 aws cloudformation deploy \
-  --template-file tomcat/main.yml \
+  --template-file main.yml \
   --stack-name "instanciadesdeCLI" \
   --capabilities CAPABILITY_NAMED_IAM
 
