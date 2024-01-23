cf9a61021b3554653586f233cdc2edeb07131291
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-23 19:05:16 +0100

    Update nuevodeploy.sh

--- nuevodeploy.sh
+++ nuevodeploy.sh
@@ -1,7 +1,4 @@
-export AWS_ACCESS_KEY_ID=
-export AWS_SECRET_ACCESS_KEY=
-export AWS_SESSION_TOKEN=
-export AWS_DEFAULT_REGION=us-east-1
+
 #desplegamos un stack y  Nombre del archivo YAML que contiene la plantilla
 # Crear o actualizar el stack
 aws cloudformation deploy \
