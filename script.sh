dfbe329e4b4f9ad689709df95c9abd9ee61b1bff
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-26 13:39:58 +0100

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#desplegamos un stack y Nombre del archivo YAML que contiene la plantilla
-# Crear o actualizar el stack
-aws cloudformation deploy \
-  --template-file tomcat/main.yml \
-  --stack-name "instanciaCLI" \
-  --capabilities CAPABILITY_NAMED_IAM
