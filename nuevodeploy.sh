c0ef804428476e25a2d24f10a5e253fd181cec35
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-01-26 13:39:22 +0100

    Delete nuevodeploy.sh

--- nuevodeploy.sh
+++
@@ -1,7 +0,0 @@
-
-#desplegamos un stack y  Nombre del archivo YAML que contiene la plantilla
-# Crear o actualizar el stack
-aws cloudformation deploy \
-  --template-file tomcat/main.yml \
-  --stack-name "instanciadesdeCLI" \
-  --capabilities CAPABILITY_NAMED_IAM
