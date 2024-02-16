8659306adb4b6f1f79aec3ea887d2f9c235991a4
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-02-16 23:43:20 +0100

    Delete deploy-ubuntu.sh

--- deploy-ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Desplegamos la plantilla de CloudFormation
-aws cloudformation deploy \
---template-file ubuntu.yml \
---stack-name "deployTomcat" \
---capabilities CAPABILITY_NAMED_IAM
