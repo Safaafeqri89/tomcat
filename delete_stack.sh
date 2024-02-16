b39d0ad065f0ad1c9e7fa2899ae8e42367ae392d
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2024-02-16 23:42:39 +0100

    Delete delete_stack.sh

--- delete_stack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Elimimar pila
-aws cloudformation delete-stack \
---stack-name "instanciadesdeCLI"  \
-# Esperar hasta que la eliminación se complete
-echo "La eliminación de la pila se ha completado."
-
-
